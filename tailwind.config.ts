1c7b36d8abeae71ca97a99a8eeb7465367c4032a
travellertope <travellertope@gmail.com>
2026-04-12 15:11:19 +0100

    Fix ticker spacing and scrolling performance

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
         },
       },
       animation: {
-        marquee: "marquee 40s linear infinite",
+        marquee: "marquee 25s linear infinite",
       },
     },
   },
