45ec429ba3cc0179db6142d05fd056d0df4d9345
travellertope <travellertope@gmail.com>
2026-04-12 13:46:24 +0100

    feat: integrate Google Analytics (GA4) with G-DNRGCXBBF4

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./editorial.css";
 import "./not-found.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -47,6 +48,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-DNRGCXBBF4" />
       </body>
     </html>
   );
