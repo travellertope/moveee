d2301eb82ef4d31d48bb115226a23e02237f3472
travellertope <travellertope@gmail.com>
2026-06-17 15:32:47 +0100

    Bump mobile version and auto-increment preview build versionCode

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -7,7 +7,7 @@ export default {
     name: "Moveee",
     slug: "moveee-platform",
     owner: "moveees-team",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
